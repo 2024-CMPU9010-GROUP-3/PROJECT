6e3763257fa96d505eb9580ab778b5c82b9f3566
YuanshuoDu <duyuanshuo@gmail.com>
2024-09-25 10:02:39 +0100

    initial google map component

--- Frontend/src/app/components/home/HomePage.tsx
+++ Frontend/src/app/components/home/HomePage.tsx
@@ -1,6 +1,14 @@
 "use client";
 
+import { GoogleMap as GoogleMapComponent } from '@react-google-maps/api';
 import { useRouter } from 'next/navigation';
+import dynamic from 'next/dynamic';
+import { useEffect } from 'react';
+
+// Dynamically import dynamic map components to prevent SSR issues
+const GoogleMap = dynamic(() => import('../../components/map/GoogleMap'), {
+  ssr: false
+});
 
 const HomePage = () => {
   const router = useRouter();
@@ -12,7 +20,8 @@ const HomePage = () => {
   return (
     <div>
       <h1>Hello Project!!!  test</h1>
-      <button onClick={handleButtonClick}>Go to Google Map</button>
+      <h2>Google Map Integration</h2>
+      <GoogleMap />
     </div>
   );
 };
