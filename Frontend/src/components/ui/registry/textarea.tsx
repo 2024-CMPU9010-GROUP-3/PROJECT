c9fec9bc1ab3271293ed39218805375235ae6e2c
YuanshuoDu <duyuanshuo@gmail.com>
2024-10-03 00:03:38 +0100

    fix bugs remove uness component and testing

--- Frontend/src/components/ui/registry/textarea.tsx
+++ Frontend/src/components/ui/registry/textarea.tsx
@@ -3,7 +3,12 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+    className?: string;
+    placeholder?: string;
+    value?: string;
+    onChange?: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  }
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
