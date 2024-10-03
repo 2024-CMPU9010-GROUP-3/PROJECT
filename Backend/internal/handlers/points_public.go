7df7adc336eb9534fabb1c613701b55ab08b9ee7
Andreas Kraus <andreas.kraus.97@gmail.com>
2024-10-03 11:47:22 +0100

    Connects points/byRadius to database, adds wrapper for row to make json encoding work

--- Backend/internal/handlers/points_public.go
+++ Backend/internal/handlers/points_public.go
@@ -6,20 +6,39 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 
 	db "github.com/2024-CMPU9010-GROUP-3/PROJECT/internal/db/public"
 	"github.com/2024-CMPU9010-GROUP-3/PROJECT/internal/util"
+	"github.com/2024-CMPU9010-GROUP-3/PROJECT/internal/wrappers"
 )
 
+const floatPrecision = 32
+
 func (p *PointsHandler) HandleGetByRadius(w http.ResponseWriter, r *http.Request) {
-	points, err := dbQueries.GetPointsInEnvelope(*dbCtx, db.GetPointsInEnvelopeParams{X1: -1, Y1: -1, X2: 1, Y2: 1})
+	// parameters long, lat, radius are required
+	params := r.URL.Query()
+	long, err_long := strconv.ParseFloat(params.Get("long"), floatPrecision)
+	lat, err_lat := strconv.ParseFloat(params.Get("lat"), floatPrecision)
+	radius, err_radius := strconv.ParseFloat(params.Get("radius"), floatPrecision)
+
+	// bad request if any parameters can't be parsed to float
+	if err_long != nil || err_lat != nil || err_radius != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	x1 := long - radius
+	y1 := lat - radius
+	x2 := long + radius
+	y2 := lat + radius
+
+	points, err := dbQueries.GetPointsInEnvelope(*dbCtx, db.GetPointsInEnvelopeParams{X1: x1, Y1: y1, X2: x2, Y2: y2})
 	if err != nil {
 		log.Printf("Could not get points from database: %v\n", err)
 	}
-	log.Printf("%v\n", points)
-	data := util.Placeholder("GET ByRadius")
 	w.Header().Set(contentType, applicationJson)
-	err = json.NewEncoder(w).Encode(data)
+	err = json.NewEncoder(w).Encode(wrappers.FromRowList(points))
 	util.CheckResponseError(err, w)
 }
 
