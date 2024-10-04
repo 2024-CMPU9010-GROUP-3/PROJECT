a6ce36ca2aecc8a39e909a3370a1366b18f36aae
YuanshuoDu <duyuanshuo@gmail.com>
2024-10-04 01:29:59 +0100

    Add UserDropDownMenu

--- Frontend/src/app/about/page.tsx
+++ Frontend/src/app/about/page.tsx
@@ -4,6 +4,7 @@ const page = () => {
   return (
     <div>
       <AboutPage />
+
     </div>
   );
 };
--- Frontend/src/app/components/about/AboutPage.tsx
+++ Frontend/src/app/components/about/AboutPage.tsx
@@ -1,4 +1,12 @@
+import { UserDropdownMenu } from "@/app/components/dropdown-menu";
+
 const AboutPage = () => {
-  return <div>All the about us content goes in this</div>;
+  return (
+    <>
+      <div>All the about us content goes in this</div>
+      <div><UserDropdownMenu/></div>
+    </>
+  );
 };
+
 export default AboutPage;
