304320efc6e537a6b7626b461e1863c63cd08aec
Andreas Kraus <andreas.kraus.97@gmail.com>
2024-10-04 17:17:31 +0100

    Adds implementation for point detail route

--- Backend/internal/handlers/points_public.go
+++ Backend/internal/handlers/points_public.go
@@ -4,6 +4,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
 	"log"
 	"net/http"
 	"strconv"
@@ -11,6 +12,7 @@ import (
 	db "github.com/2024-CMPU9010-GROUP-3/PROJECT/internal/db/public"
 	"github.com/2024-CMPU9010-GROUP-3/PROJECT/internal/util"
 	"github.com/2024-CMPU9010-GROUP-3/PROJECT/internal/wrappers"
+	"github.com/jackc/pgx/v5"
 )
 
 const floatPrecision = 32
@@ -28,6 +30,7 @@ func (p *PointsHandler) HandleGetByRadius(w http.ResponseWriter, r *http.Request
 		return
 	}
 
+	// construct envelope
 	x1 := long - radius
 	y1 := lat - radius
 	x2 := long + radius
@@ -43,9 +46,38 @@ func (p *PointsHandler) HandleGetByRadius(w http.ResponseWriter, r *http.Request
 }
 
 func (p *PointsHandler) HandleGetPointDetails(w http.ResponseWriter, r *http.Request) {
-	data := util.Placeholder("GET PointDetails")
+	pointIdPathParam := r.PathValue("id")
+	pointId, err := strconv.ParseInt(pointIdPathParam, 10, 64)
+
+	// bad request if id can't be parsed to int
+	if err != nil {
+		log.Printf("Invalid path parameter: %v\n", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	pointDetailsBytes, err := dbQueries.GetPointDetails(*dbCtx, pointId)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		} else {
+			log.Printf("Could not get point details from database: %+v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	}
+
+	// check if bytes received from database are valid json
+	var decodedDetails json.RawMessage
+	err = json.Unmarshal(pointDetailsBytes, &decodedDetails)
+	if err != nil {
+		log.Printf("Received invalid json from database: %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
 	w.Header().Set(contentType, applicationJson)
-	w.WriteHeader(http.StatusOK)
-	err := json.NewEncoder(w).Encode(data)
+	err = json.NewEncoder(w).Encode(decodedDetails)
 	util.CheckResponseError(err, w)
 }
