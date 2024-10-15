a1631874ee659ef036b6f69d9659e0dadef0db76
Andreas Kraus <andreas.kraus.97@gmail.com>
2024-10-15 18:24:52 +0100

    Adds optional parameter Cause to CustomError for passing in underlying errors

--- Backend/internal/errors/errors.go
+++ Backend/internal/errors/errors.go
@@ -4,4 +4,10 @@ type CustomError struct {
 	HttpStatus int
 	ErrorCode  int    `json:"errorCode"`
 	ErrorMsg   string `json:"errorMsg"`
+	Cause      string `json:"cause"`
+}
+
+func (c CustomError) WithCause(err error) CustomError {
+	c.Cause = err.Error()
+	return c
 }
