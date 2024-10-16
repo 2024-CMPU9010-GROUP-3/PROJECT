083a1dd709ccdee7cd0db6969772cbe4c041e5bc
Andreas Kraus <andreas.kraus.97@gmail.com>
2024-10-16 13:57:03 +0100

    Adds cors policy needed for login

--- Backend/cmd/main/main.go
+++ Backend/cmd/main/main.go
@@ -69,6 +69,7 @@ func main() {
 	corsManager := cors.New(cors.Options{
 		AllowedOrigins: allowedOrigins,
 		AllowedMethods: allowedMethods,
+		AllowCredentials: true, // needed for login
 	})
 
 	middlewares := middleware.CreateMiddlewareStack(
