2449854f68b5ca16bde6878e2970da9cdc7f608e
Andreas Kraus <andreas.kraus.97@gmail.com>
2024-10-18 17:57:27 +0100

    Removes dummy code

--- Backend/internal/routes/routes_private_test.go
+++ Backend/internal/routes/routes_private_test.go
@@ -1,20 +1,3 @@
 //go:build private
 
 package routes
-
-import (
-	"fmt"
-	"testing"
-)
-
-func TestA(t *testing.T) {
-	t.Error("failing test")
-}
-
-func TestB(t *testing.T) {
-	t.Skip("skipping test")
-}
-
-func TestC(t *testing.T) {
-	fmt.Println("succeeding test")
-}
