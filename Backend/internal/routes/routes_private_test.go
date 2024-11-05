4a2a49396b6e82b25f7af09f2747be1d0f0d2779
Andreas Kraus <andreas.kraus.97@gmail.com>
2024-11-05 14:45:27 +0000

    Adds unit tests for private router

--- Backend/internal/routes/routes_private_test.go
+++ Backend/internal/routes/routes_private_test.go
@@ -1,3 +1,42 @@
 //go:build private
 
 package routes
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/2024-CMPU9010-GROUP-3/magpie/internal/util/testutil"
+)
+
+
+func TestPrivateRoutes(t *testing.T) {
+	tests := []testutil.RouterTestDefinition{
+		{
+			Name:               "Test POST /points/",
+			Path:               "/points/",
+			Method:             "POST",
+			ExpectedStatusCode: http.StatusBadRequest,
+		},
+		{
+			Name:               "Test PUT /points/{id}",
+			Path:               "/points/123",
+			Method:             "PUT",
+			ExpectedStatusCode: http.StatusBadRequest,
+		},
+		{
+			Name:               "Test DELETE /points/{id}",
+			Path:               "/points/123",
+			Method:             "DELETE",
+			ExpectedStatusCode: http.StatusBadRequest,
+		},
+		{
+			Name:               "Test non-existing private route",
+			Path:               "/points/nonexistent",
+			Method:             "GET",
+			ExpectedStatusCode: http.StatusNotFound,
+		},
+	}
+
+	testutil.RunRouterTests(t, tests, private())
+}
