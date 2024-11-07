fixes calculation of average width/length

Python/script/parking-detection/parking_detection.py
@@ -295,27 +295,6 @@ def get_center_bounding_box(x_min, y_min, x_max, y_max):
 
     return x, y
 
-def calculate_meters_per_pixel(latitude, zoom_level=18):
-    """
-    Calculates how many meters per pixel at a certain latitude and zoom level
-    
-    Params:
-        latitude (float): The latitude value
-        zoom_level (int): The zoom level of the mapbox image
-        
-    Returns:
-        adjusted_meters_per_pixel (float): Returns the adjusted_meters_per_pixel at the latitude and zoom level
-    """
-    earth_circumference = 40075017
-    meters_per_pixel = earth_circumference / (256 * 2**zoom_level)
-
-    cos_latitude = math.cos(math.radians(latitude))
-    if cos_latitude < 0:
-        cos_latitude = abs(cos_latitude)
-
-    adjusted_meters_per_pixel = meters_per_pixel * cos_latitude
-    return abs(adjusted_meters_per_pixel)
-
 def calculate_avg_spot_dimensions(cars):
     """
     Calculates average parking spot width and length from the bounding box found as it is very variable
@@ -328,16 +307,13 @@ def calculate_avg_spot_dimensions(cars):
     """
     widths = [car[2] for car in cars]
     lengths = [car[3] for car in cars]
-    latitudes = [car[1] for car in cars]
     avg_width_pixels = np.mean(widths)
     avg_length_pixels = np.mean(lengths)
-    avg_latitude = np.mean(latitudes)
 
-    adjusted_meters_per_pixel = calculate_meters_per_pixel(avg_latitude)
-    print(adjusted_meters_per_pixel)
+    #previous spot_width_meters=2, spot_length_meters=3, spot_width=18, spot_length =32
 
-    avg_width_meters = avg_width_pixels * adjusted_meters_per_pixel
-    avg_length_meters = avg_length_pixels * adjusted_meters_per_pixel
+    avg_width_meters = 2 * avg_width_pixels / 18
+    avg_length_meters = 3* avg_length_pixels / 32
 
     print(avg_width_meters, avg_length_meters, avg_width_pixels, avg_length_pixels)
     return avg_width_meters, avg_length_meters, avg_width_pixels, avg_length_pixels
@@ -411,15 +387,15 @@ def draw_empty_spots_on_image(image_path, empty_spots, center_long, center_lat, 
         x_pixel, y_pixel = convert_coordinates_to_bounding_box(spot[0], spot[1], center_long, center_lat)
         
         if orientation == 'horizontal':
-            x1 = int(x_pixel - avg_spot_length // 2)
-            y1 = int(y_pixel - avg_spot_width // 2)
-            x2 = int(x_pixel + avg_spot_length // 2)
-            y2 = int(y_pixel + avg_spot_width // 2)            
-        else: 
             x1 = int(x_pixel - avg_spot_width // 2)
             y1 = int(y_pixel - avg_spot_length // 2)
             x2 = int(x_pixel + avg_spot_width // 2)
             y2 = int(y_pixel + avg_spot_length // 2)
+        else: 
+            x1 = int(x_pixel - avg_spot_length // 2)
+            y1 = int(y_pixel - avg_spot_width // 2)
+            x2 = int(x_pixel + avg_spot_length // 2)
+            y2 = int(y_pixel + avg_spot_width // 2)  
         
         cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
@@ -593,10 +569,10 @@ def main(top_left_longitude, top_left_latitude, bottom_right_longitude, bottom_r
     
 
 if __name__ == "__main__":
-    #main(-6.2576, 53.3388, -6.2566, 53.3394)
-    #main(-6.2608, 53.3464, -6.2598, 53.347)
-    #main(-6.2617, 53.3462, -6.2606, 53.3469)
-    #main(-6.2854, 53.3511, -6.2843, 53.3517)
-    #main(-6.2893, 53.3486, -6.2883, 53.3492)
-    #main(-6.2899, 53.3473, -6.2889, 53.3479)
+    main(-6.2576, 53.3388, -6.2566, 53.3394)
+    main(-6.2608, 53.3464, -6.2598, 53.347)
+    main(-6.2617, 53.3462, -6.2606, 53.3469)
+    main(-6.2854, 53.3511, -6.2843, 53.3517)
+    main(-6.2893, 53.3486, -6.2883, 53.3492)
+    main(-6.2899, 53.3473, -6.2889, 53.3479)
     main(-6.2903, 53.349, -6.2893, 53.3496)