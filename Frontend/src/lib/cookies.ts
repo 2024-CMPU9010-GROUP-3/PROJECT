1e6aab46ad86a43f535ed372f8f5524bc2c76317
Andreas Kraus <andreas.kraus.97@gmail.com>
2024-11-15 13:21:48 +0000

    Adds expiry to cookies accepted cookie

--- Frontend/src/lib/cookies.ts
+++ Frontend/src/lib/cookies.ts
@@ -3,6 +3,7 @@
 import {cookies} from "next/headers";
 
 const cookiesAcceptedName = "magpie_cookies_accepted"
+const cookiesAcceptedMaxAge = 365 * 24 * 60 * 60; // one year
 
 export async function getCookiesAccepted() {
   const cookieStore = cookies()
@@ -16,7 +17,11 @@ export async function getCookiesAccepted() {
 
 export async function setCookiesAccepted(){
   const cookieStore = cookies();
-  cookieStore.set(cookiesAcceptedName, "true");
+  cookieStore.set({
+    name: cookiesAcceptedName,
+    value: "true",
+    maxAge: cookiesAcceptedMaxAge,
+  });
 }
 
 export async function unsetCookiesAccepted() {
