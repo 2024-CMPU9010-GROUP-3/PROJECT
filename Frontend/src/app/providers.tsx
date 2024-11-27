a5e5eaa974a07b1b4d996872b8dabedf7f3c7fcd
Kaustubh Trivedi <kaus12tri@gmail.com>
2024-11-27 01:43:46 +0000

    fixed merge conflicts from main

--- Frontend/src/app/providers.tsx
+++ Frontend/src/app/providers.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { getQueryClient } from "@/app/get-query-client";
 import type * as React from "react";
 
@@ -8,9 +7,6 @@ export default function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = getQueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
