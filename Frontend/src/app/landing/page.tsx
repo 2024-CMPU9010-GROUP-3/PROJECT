5eaeb639c71b9bbd39029feea2d0c4e2f1bfa742
Kaustubh Trivedi <kaus12tri@gmail.com>
2024-11-28 12:18:09 +0000

    stripped out navbar from landing page component to make it global

--- Frontend/src/app/landing/page.tsx
+++ Frontend/src/app/landing/page.tsx
@@ -1,7 +1,7 @@
+import { Navbar } from "../../components/global/Navbar";
 import { CTA } from "./CTA";
 import { Features } from "./Features";
 import HeroSection from "./HeroSection";
-import { Navbar } from "./Navbar";
 import { UseCases } from "./UseCases";
 import WhatIsMagpie from "./WhatIsMagpie";
 const page = () => {
