01c25325b3ba48eafe4ab1f83414c87fb3653042
Andreas Kraus <andreas.kraus.97@gmail.com>
2024-12-02 13:19:01 +0000

    Fixes build error

--- Frontend/src/app/components/home/InnerHomePage.tsx
+++ Frontend/src/app/components/home/InnerHomePage.tsx
@@ -2,11 +2,15 @@ import LocationAggregatorMap from "../map/MapboxMap";
 import ProtectedRoute from "../ProtectedRoute";
 import Nav from "@/app/nav/page";
 import CookieConsent from "../banner/CookieConsent";
+import {Suspense} from "react";
+import {Loader2} from "lucide-react";
 
 const InnerHomePage = () => {
   return (
     <ProtectedRoute>
-      <LocationAggregatorMap />
+      <Suspense fallback={<Loader2/>}>
+        <LocationAggregatorMap />
+      </Suspense>
       <div className="nav-container">
         <div className="absolute top-5 left-5 z-[999]">
           <CookieConsent />
