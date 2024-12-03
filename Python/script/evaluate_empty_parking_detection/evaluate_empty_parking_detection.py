88030d9d73c0c0a0f2549f5533384b11860d69c8
Jessica <jess68400@gmail.com>
2024-12-03 14:55:43 +0000

    fixes the metrics calculations as before when there were no true labels or predictions it would add 0 for metrics

--- Python/script/evaluate_empty_parking_detection/evaluate_empty_parking_detection.py
+++ Python/script/evaluate_empty_parking_detection/evaluate_empty_parking_detection.py
@@ -742,6 +742,9 @@ def main(directory, output_file="metrics.csv"):
     for long, lat in set(coordinates):
         predictions = get_predictions_in_image(model, long, lat, directory)
         true_labels = get_true_labels(long, lat, directory)
+        #true_labels = get_true_labels_automatic_orientation_labelling(long, lat, directory)
+        if not predictions and not true_labels:# when there are no detections and no true labels we want to skip the calculation of the metrics
+            continue
         draw_true_labels(true_labels, directory, long, lat)
         #print(predictions)
         #print(true_labels)
